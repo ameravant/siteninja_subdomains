7d56603d6a976aa65f293d1cf5c8b2a32a5e3e39
jason gagne <jgagne33@gmail.com>
2010-08-27 15:34:57 -0700

    fixing things

--- init.rb
+++ init.rb
@@ -1,11 +1,10 @@
-require 'paperclip'
 require 'subdomainify'
 require 'permalink_fu_replacement.rb'
 require 'validation_ext.rb'
-for klass in %w(Event Person User Page Menu Article ArticleCategory PersonGroup Feature FeaturableSection Testimonial Gallery Image)  
+for klass in %w(Event Image Person User Page Menu Article ArticleCategory PersonGroup Feature FeaturableSection Testimonial Gallery)  
   if ActiveRecord::Base.connection.tables.include?(klass.downcase.pluralize.tableize)
     klass.constantize.send(:subdomainify)
   end
 end
-ActiveRecord::Base::Page.send(:uniqueness_validation_for_meta_title)
-ActiveRecord::Base::Person.send(:person_extra_methods)
+Page.send(:uniqueness_validation_for_meta_title)
+Person.send(:person_extra_methods)
