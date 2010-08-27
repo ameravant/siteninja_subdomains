39457661a2512ff1acdd62477db1e93ae5f6058f
jason gagne <jgagne33@gmail.com>
2010-08-27 12:51:53 -0700

    adjusting for existing tables

--- db/migrate/20100817192647_add_account_id_to_events.rb
+++ db/migrate/20100817192647_add_account_id_to_events.rb
@@ -1,13 +1,17 @@
 class AddAccountIdToEvents < ActiveRecord::Migration
   def self.up
     for klass in [:events, :people, :person_groups, :users, :pages, :menus, :articles, :article_categories, :features, :featurable_sections, :testimonials, :galleries, :images]
-      add_column klass, :account_id, :integer, :default => 1
+      if ActiveRecord.connection.tables.include?(klass.to_s.downcase.pluralize)
+        add_column klass, :account_id, :integer, :default => 1
+      end
     end
   end
 
   def self.down
     for klass in [:events, :people, :person_groups, :users, :pages, :menus, :articles, :article_categories, :features, :featurable_sections, :testimonials, :galleries, :images]
-      remove_column klass, :account_id
+      if ActiveRecord.connection.tables.include?(klass.to_s.downcase.pluralize)
+        remove_column klass, :account_id
+      end
     end
   end
 end
--- init.rb
+++ init.rb
@@ -2,7 +2,9 @@ require 'subdomainify'
 require 'permalink_fu_replacement.rb'
 require 'validation_ext.rb'
 for klass in [Event, Person, User, Page, Menu, Article, ArticleCategory, PersonGroup, Feature, FeaturableSection, Testimonial, Gallery, Image]  
-  klass.send(:subdomainify)
+  if ActiveRecord.connection.tables.include?(klass.to_s.downcase.pluralize)
+    klass.send(:subdomainify)
+  end
 end
 Page.send(:uniqueness_validation_for_meta_title)
 Person.send(:person_extra_methods)
