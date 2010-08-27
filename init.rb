956e1ce58bd95bb22c4494bb718bd33444712ff5
jason gagne <jgagne33@gmail.com>
2010-08-27 14:25:19 -0700

    fixing issue

--- init.rb
+++ init.rb
@@ -2,8 +2,8 @@ require 'subdomainify'
 require 'permalink_fu_replacement.rb'
 require 'validation_ext.rb'
 for klass in %w(Event Person User Page Menu Article ArticleCategory PersonGroup Feature FeaturableSection Testimonial Gallery Image)  
-  if ActiveRecord::Base.connection.tables.include?(klass.tableize)
-    klass.send(:subdomainify)
+  if ActiveRecord::Base.connection.tables.include?(klass.downcase.pluralize.tableize)
+    klass.constantize.send(:subdomainify)
   end
 end
 Page.send(:uniqueness_validation_for_meta_title)
