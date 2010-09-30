3340806217de21445f1ab517b68246733996e5da
jason gagne <jgagne33@gmail.com>
2010-09-30 10:56:10 -0700

    fixing constants

--- db/migrate/20100906192647_add_account_id_to_klasses.rb
+++ db/migrate/20100906192647_add_account_id_to_klasses.rb
@@ -1,16 +1,16 @@
 class AddAccountIdToKlasses < ActiveRecord::Migration
   def self.up
-    for klass in Klasses
-      if ActiveRecord::Base.connection.tables.include?(klass.table_name)
-        add_column klass.table_name.to_sym, :account_id, :integer, :default => 1
+    for table_name in TableNames
+      if ActiveRecord::Base.connection.tables.include?(table_name)
+        add_column table_name.to_sym, :account_id, :integer, :default => 1
       end
     end
   end
 
   def self.down
-    for klass in Klasses
-      if ActiveRecord::Base.connection.tables.include?(klass.table_name)
-        remove_column klass.table_name.to_sym, :account_id
+    for table_name in TableNames
+      if ActiveRecord::Base.connection.tables.include?(table_name)
+        remove_column table_name.to_sym, :account_id
       end
     end
   end
--- lib/subdomainify/konstants.rb
+++ lib/subdomainify/konstants.rb
@@ -1,8 +1,8 @@
 module Konstants
-  tablenames = %w(article_categories articles assets column_sections comments emails event_categories 
+  TableNames = %w(article_categories articles assets column_sections comments emails event_categories 
                    event_price_options event_registrations event_transactions events featurable_sections
                    features folders galleries images inquiries link_categories links menus newsletter_blasts 
                    newsletters pages people person_groups product_categories product_options products 
                    profiles redirects searches settings taggings testimonials users)
-  Klasses = tablenames.reject{|t| !ActiveRecord::Base.connection.tables.include?(t)}.collect{|c| c.camelcase.singularize.constantize}
+  Klasses = TableNames.reject{|t| !ActiveRecord::Base.connection.tables.include?(t)}.collect{|c| c.camelcase.singularize.constantize}
 end
